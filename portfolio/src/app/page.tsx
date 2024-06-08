c002f904dcc0d6ec00554ce47c6b5b884c322633
Pankhuri Gupta <34907504+pankhuri0209@users.noreply.github.com>
2024-06-07 22:55:19 -0400

    commit card

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar"; // Ensure this path is correct
-
+import { Card, CardBody, Button, Slider } from "@nextui-org/react";
 export default function Home() {
   return (
     <div>
@@ -43,6 +43,34 @@ export default function Home() {
             height={300}
           />
         </div>
+      </div>
+
+      <div className="flex justify-center mt-16 w-full">
+        <Card
+          isBlurred
+          className="border-none bg-background/60 dark:bg-default-100/50 w-full max-w-[610px] rounded-none"
+          shadow="sm"
+        >
+          <CardBody>
+            <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
+              <div className="relative col-span-6 md:col-span-4"></div>
+
+              <div className="flex flex-col col-span-6 md:col-span-8">
+                <div className="flex justify-between items-start">
+                  <div className="flex flex-col gap-0">
+                    <h3 className="font-semibold text-foreground/90">
+                      Daily Mix
+                    </h3>
+                    <p className="text-small text-foreground/80">12 Tracks</p>
+                    <h1 className="text-large font-medium mt-2">
+                      Frontend Radio
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </CardBody>
+        </Card>
       </div>
     </div>
   );
